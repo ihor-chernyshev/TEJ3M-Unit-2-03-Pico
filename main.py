976aa1c8c53f58a7d6ce92881cd342d8902920a1
ihor-chernyshev <144264214+ihor-chernyshev@users.noreply.github.com>
2025-03-03 14:48:51 +0000

    Added main.py program

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
-"""Created by Ihor Chernyshev"""
-"""Created on Feb 2025"""
-"""This program turns an LED on for one second, repeatedly, but the interval increases each time by 1 more second"""
+"""
+Created by Ihor Chernyshev
+Created on Feb 2025
+This program turns an LED on for one second, repeatedly, but the interval increases each time by 1 more second
+"""
+
 
 import time
 import board
 import digitalio
 
+
 blink_time = 1
-led = digitalio.DigitalInOut(board.GP14)
+led = digitalio.DigitalInOut(board.GP15)
 led.direction = digitalio.Direction.OUTPUT
 
+# Turns LED on and off
 while True:
     led.value = True
     time.sleep(blink_time)
